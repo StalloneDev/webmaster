ca91d9e3cfef2b19941fb21520358d6bf17e2449
StalloneDev <139977747+StalloneDev@users.noreply.github.com>
2024-02-06 13:36:18 +0000

    six commit

--- Webmaster/settings.py
+++ Webmaster/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Webmaster.urls'
@@ -121,7 +122,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
